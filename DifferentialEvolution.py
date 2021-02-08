bde303f76a7ec9cc1a0204881eb27ea39eed1cba
Sebastjan <sebastjancizel@gmail.com>
2021-02-08 19:53:51 +0100

    Added visual exploration of differential evolution in Jupyter notebook.

--- DifferentialEvolution.py
+++ DifferentialEvolution.py
@@ -1,15 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
+from mutations import current_to_best_mutation, rand_mutation
 
- #TODO: Visualize the steps of Differential Evolution
- #TODO: Implement variants of the algorithm (have separate mutation and recombination methods in the class)
- #TODO: Try to use cython to speed this up?
+# TODO: Visualize the steps of Differential Evolution
+# TODO: Implement variants of the algorithm (have separate mutation and recombination methods in the class)
 
 
 class DifferentialEvolution:
-
     def __init__(self, f, limits, seed=None):
         """
             Expects limits as list of intervals
@@ -19,7 +16,8 @@ class DifferentialEvolution:
         self.limits = np.asarray(limits).T
         self.seed = seed
         self.fitness_history = None
-
+        self.generation_history = None
+        self.best_in_generation = None
 
     def _rescale(self, arr):
         lower, upper = self.limits
@@ -38,30 +36,48 @@ class DifferentialEvolution:
 
         current = population[current_idx]
         best = population[best_idx]
-        mutant = current + mut * (best - current) + mut * (x1 - x2)
-
-        return np.clip(mutant, 0, 1)
+        return current_to_best_mutation(current, best, x1, x2, mut)
 
     @staticmethod
-    def _rand_mutation(mut, current_idx, population):
+    def _rand_mutation(mut, current_idx, best_idx, population):
         popsize = len(population)
         idxs = [idx for idx in range(popsize) if idx != current_idx]
 
         x1, x2, x3 = population[np.random.choice(idxs, size=3, replace=False)]
-        mutant = x1 + mut * (x2 - x3)
 
-        return np.clip(mutant, 0, 1)
+        return rand_mutation(x1, x2, x3, mut)
 
+    def optimize(
+        self,
+        popsize=20,
+        iterations=100,
+        mut=0.5,
+        crossp=0.7,
+        strategy="curr_to_best",
+        generation_history=False,
+        track_best=False,
+        initial_pop=None
+    ):
 
+        STRATEGIES = {
+            "rand_mutation": self._rand_mutation,
+            "curr_to_best": self._current_to_best_mutation,
+        }
 
-    def optimize(self, popsize=20, iterations=100, mut=0.5, crossp=0.7):
+        mutation_function = STRATEGIES.get(strategy, "curr_to_best")
+
         np.random.seed(self.seed)
         dims = self.dimensions
-
         fitness_history = np.zeros(iterations)
 
         # Generate and rescale initial population
-        pop = np.random.rand(popsize, dims)
+
+        if initial_pop is not None:
+            pop = initial_pop
+            popsize = len(pop)
+        else:
+            pop = np.random.rand(popsize, dims)
+
         pop_resc = self._rescale(pop)
 
         # Compute initial fitness function
@@ -71,16 +87,21 @@ class DifferentialEvolution:
         best_idx = np.argmin(fitness)
         best = pop_resc[best_idx]
 
+        if generation_history:
+            self.generation_history = [pop_resc]
+
+        if track_best:
+            self.best_in_generation = [best]
+
         for i in range(iterations):
             for j in range(popsize):
-                #mutant = self._rand_mutation(mut, j, pop)
-                mutant = self._current_to_best_mutation(mut, j, best_idx, pop)
-
+                # mutant = self._rand_mutation(mut, j, pop)
+                mutant = mutation_function(mut, j, best_idx, pop)
 
                 cross_points = np.random.rand(dims) < crossp
 
                 if not np.any(cross_points):
-                    cross_points[np.random.randint(0,dims)] = True
+                    cross_points[np.random.randint(0, dims)] = True
 
                 trial = np.where(cross_points, mutant, pop[j])
                 trial_resc = self._rescale(trial)
@@ -96,13 +117,17 @@ class DifferentialEvolution:
 
             fitness_history[i] = fitness[best_idx]
 
-        self.fitness_history = fitness_history
+            if track_best:
+                self.best_in_generation.append(list(best))
+            if generation_history:
+                self.generation_history.append(list(self._rescale(pop)))
 
+        self.fitness_history = fitness_history
 
         return best, fitness[best_idx]
 
-class Testing:
 
+class Testing:
     def __init__(self, limits=None, **kwargs):
         self.limits = limits
         self.params = kwargs
@@ -111,7 +136,7 @@ class Testing:
     def plot_fitness(fitness_history):
         x = [i for i in range(len(fitness_history))]
         y = fitness_history
-        plt.plot(x,y)
+        plt.plot(x, y)
         plt.show()
 
     def plot_function(self, f):
@@ -121,66 +146,50 @@ class Testing:
         x = np.arange(x0, x1, 0.1)
         y = np.arange(y0, y1, 0.1)
 
-        xs, ys = np.meshgrid(x,y)
-        z = [f([u,v]) for u,v in zip(xs,ys)]
+        xs, ys = np.meshgrid(x, y)
+        z = [f([u, v]) for u, v in zip(xs, ys)]
 
-        plt.contourf(x,y,z)
+        plt.contourf(x, y, z)
         plt.show()
 
-    def quadraticTest(self, plot=False ):
+    def quadraticTest(self, plot=False):
         print("Starting test for the function f(x,y) = x^2 + y^2")
-        print("="*50)
+        print("=" * 50)
         f = lambda x: sum(i ** 2 for i in x)
         problem = DifferentialEvolution(f, self.limits)
         pt, value = problem.optimize(**self.params)
-        print(f"Minimum is at:\t\t\t {np.round(pt,decimals=6)}")
-        print(f"Minimal value is:\t\t {np.round(value, decimals=6)}.")
-        print("="*50)
+        print(f"Minimum is at:\t\t\t {np.round(pt,decimals=10)}")
+        print(f"Minimal value is:\t\t {np.round(value, decimals=10)}.")
+        print("=" * 50)
         if plot:
             self.plot_function(f)
             self.plot_fitness(problem.fitness_history)
 
-
     def ackleyTest(self, plot=False):
         def ackley_function(x):
             x1, x2 = x
-            #returns the point value of the given coordinate
-            part_1 = -0.2*np.sqrt(0.5*(x1*x1 + x2*x2))
-            part_2 = 0.5*(np.cos(2*np.pi*x1) + np.cos(2*np.pi*x2))
-            value = np.exp(1) + 20 -20*np.exp(part_1) - np.exp(part_2)
-            #returning the value
+            # returns the point value of the given coordinate
+            part_1 = -0.2 * np.sqrt(0.5 * (x1 * x1 + x2 * x2))
+            part_2 = 0.5 * (np.cos(2 * np.pi * x1) + np.cos(2 * np.pi * x2))
+            value = np.exp(1) + 20 - 20 * np.exp(part_1) - np.exp(part_2)
+            # returning the value
             return value
 
         print("Starting test for the Ackley function.")
-        print("="*50)
+        print("=" * 50)
         problem = DifferentialEvolution(ackley_function, self.limits)
         pt, value = problem.optimize(**self.params)
-        print(f"Minimum is at:\t\t\t {np.round(pt,decimals=6)}")
-        print(f"Minimal value is:\t\t {np.round(value, decimals=6)}.")
-        print("="*50)
+        print(f"Minimum is at:\t\t\t {np.round(pt,decimals=10)}")
+        print(f"Minimal value is:\t\t {np.round(value, decimals=10)}.")
+        print("=" * 50)
         if plot:
             self.plot_function(ackley_function)
             self.plot_fitness(problem.fitness_history)
 
 
+if __name__ == "__main__":
+    limits = [(-50, 50)] * 2
 
-
-
-
-
-if __name__ == '__main__':
-    limits = [(-5,5)]*2
-
-    qtest = Testing(limits)
-    qtest.quadraticTest(plot=True)
-    qtest.ackleyTest(plot=True)
-
-
-
-
-
-
-
-
-
-
+    qtest = Testing(limits, popsize=100, iterations=100)
+    qtest.quadraticTest(plot=False)
+    qtest.ackleyTest(plot=False)
--- mutations.pyx
+++ mutations.pyx
@@ -1,5 +1,6 @@
 import numpy as np
 cimport cython
+from cython.parallel cimport prange
 
 #cython: boundscheck=False, wraparound=False
 
@@ -7,35 +8,33 @@ ctypedef fused floating:
     float
     double
 
-
-cdef floating clip(floating value):
+cdef floating clip(floating value) nogil:
     return min(max(value, 0), 1)
 
 def current_to_best_mutation(floating[:] current, floating[:] best, floating[:] x1, floating[:] x2, floating mut):
+
     cdef Py_ssize_t x_max = current.shape[0]
-
     cdef Py_ssize_t x
-    cdef double temp
-    result = np.zeros((x_max,), dtype = np.float_)
+    cdef floating temp
+    cdef floating[:] result = np.empty(x_max, dtype = np.float_)
 
-    for x in range(x_max):
+    for x in prange(x_max,nogil=True):
         temp = current[x] + mut * (best[x] - current[x]) + mut * (x1[x] - x2[x])
         result[x] = clip(temp)
 
-    return result
-
+    return np.array(result)
 
 def rand_mutation(floating[:] x1, floating[:] x2, floating[:] x3, floating mut):
 
     cdef Py_ssize_t x_max = x1.shape[0]
     cdef Py_ssize_t x
-    cdef double temp
+    cdef floating temp
+    cdef floating[:] result = np.empty(x_max, dtype=np.float_)
 
-    result = np.zeros((x_max,), dtype=np.float_)
-    for x in range(x_max):
+    for x in prange(x_max, nogil=True):
         temp = x1[x] + mut*(x2[x] - x3[x])
         result[x] = clip(temp)
 
-    return result
+    return np.array(result)
 
 
